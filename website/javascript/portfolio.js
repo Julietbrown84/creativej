f2a516c4788c329fecbd89b9249d156fccd4ff4b
Macintoshi HD <MacBook@Macintoshis-MBP.station>
2015-05-31 01:06:39 +1200

    snow effects added

--- website/javascript/portfolio.js
+++ website/javascript/portfolio.js
@@ -1,23 +1,3 @@
-$( document ).ready(function() {
-
-  $("#image").hide()
-
-  setInterval(function(){
-    pokerain()
-  }, 150);
-
-  function pokerain() {
-    var pokeclone = $("#image").clone().show();
-    var left_offset = Math.floor((Math.random() * 90) + 1);
-    $(".image_rain").append(pokeclone);
-    $(pokeclone).parent().css({position: 'relative'});
-    $(pokeclone).css({left: left_offset + "%", top: -100, position:'absolute'});
-    $(pokeclone).animate({ "top": "600px" }, 2300 );
-  }
-
-  $("#random").click(function() {
-    var pokerandom = Math.floor((Math.random() * 719) + 1);
-    window.location = '/' + pokerandom;
-  });
-
-})
+$(document).ready( function(){
+  $.fn.snow({ minSize: 5, maxSize: 50, newOn: 1000, flakeColor: ‘#0099FF’ });
+});
