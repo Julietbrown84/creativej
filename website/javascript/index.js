b7975b70c6b783d41ac68526f950bd8e0d83b12e
Macintoshi HD <MacBook@Macintoshis-MBP.station>
2015-05-31 16:18:05 +1200

    sorting out cloud

--- website/javascript/index.js
+++ website/javascript/index.js
@@ -7,7 +7,6 @@ $(window).ready(function() {
 // 
 
 var main = function() {
-  /* Push the body and the nav over by 285px over */
   $('.icon-menu').click(function() {
     $('.menu').animate({
       left: "0px"
