1f03e2fdbac5baa38efdb5556289aa7fc2815395
Juliet Brown <apprentice@StMac4.local>
2015-06-03 11:28:46 +1200

    added html div'

--- website/javascript/animation.js
+++ website/javascript/animation.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     $('button').click(function() {
       $('div').fadeOut('slow')
     });
-
+    $('div').click(function() {
+      $(this).fadeOut('slow')
+    });
     $('.red, .pink').fadeTo('slow', 0)
 });
